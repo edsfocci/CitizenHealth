4fc1a4034211eb1464ecf1ab657fb66590edc0cb
Ed Solis <edsfocci@gmail.com>
2013-11-11 15:38:30 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitizenHealth::Application.config.secret_token = '7e85bb2e846a26f646ebcc65604b782d88ca1c0846f108fcffc21b9c226afe7fa4f090cc36646c24a09d7ed3ec96ee53c1e0edf1a97a280c4d5dc6afb57d43f8'
